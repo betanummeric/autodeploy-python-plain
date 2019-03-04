2fe69c048d77c2b6fb3a2b92cdab0db70ecb0892
Felix Hamme <fhamme@united-internet.de>
2019-03-04 15:19:29 +0100

    create index.py, a hello world

---
+++ index.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import sys
+
+print("Content-Type: text/plain")
+print()
+print("I'm a Hello World written in plain python!")
+print("https://github.com/betanummeric/autodeploy-python-plain.git")
